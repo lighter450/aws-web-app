923b84ab620c08ef6bc864e61a1eb0782fcc1326
Bohdan Ponikar <bohdan.ponikar@nure.ua>
2025-06-20 18:46:59 +0300

    Test Terraform 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,20 +4,27 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 для us-east-1
   instance_type = "t2.micro"
   key_name      = "keylab1"               # заміни на назву свого .pem ключа
+  
   tags = {
     Name = "Terraform-WebApp"
   }
-
+  
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 }
 
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
   description = "Allow HTTP"
+  vpc_id      = data.aws_vpc.default.id  # Add this line
   
   ingress {
     from_port   = 80
@@ -25,7 +32,7 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   egress {
     from_port   = 0
     to_port     = 0
@@ -33,4 +40,3 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
