d27cb6114aa1babfa9a3e03c616c32f703441acd
Bohdan Ponikar <bohdan.ponikar@nure.ua>
2025-06-20 19:18:46 +0300

    Test Terraform 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,23 +20,3 @@ resource "aws_instance" "web" {
   
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 }
-
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP"
-  vpc_id      = data.aws_vpc.default.id  # Add this line
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
