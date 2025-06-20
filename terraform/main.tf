d41442f8fc027184a4bc6189a40e239870fab0b8
Bohdan Ponikar <bohdan.ponikar@nure.ua>
2025-06-20 17:45:55 +0300

    Add Terraform CI/CD pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 для us-east-1
+  instance_type = "t2.micro"
+  key_name      = "keylab1"               # заміни на назву свого .pem ключа
+  tags = {
+    Name = "Terraform-WebApp"
+  }
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
